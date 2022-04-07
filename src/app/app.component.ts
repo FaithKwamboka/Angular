f8224e949ecc3bc452121084e6019981a1c64ec3
FaithKwamboka <okongofaith3@gmail.com>
2022-04-07 16:59:19 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Goal } from './goal';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title="MY GOALS"
+  // goals: Goal[] = [
+  //   new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date),
+  //   new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date),
+  //   new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date),
+  //   new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date),
+  //   new Goal(5,'Solve math homework','Damn Math',new Date),
+  //   new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date),
+  // ];
+
+}
